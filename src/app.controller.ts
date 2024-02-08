8aa2b4fd43b4ca098b8ec37810e13cda8b11b282
Michel Balvin Ormedo <michelbalvinor@gmail.com>
2024-02-08 10:14:09 -0500

    prueba3.7

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards,HttpStatus} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-
+interface AuthenticatedRequest extends Request {
+  user: any; // Define el tipo de 'user' según lo que esperas recibir
+}
 @Controller()
 export class AppController {
   constructor() {}
@@ -8,6 +10,20 @@ export class AppController {
   @Get()
   getHello(): string {
     return 'Hello World!';
+  }
+  @Get("/facebook")
+  @UseGuards(AuthGuard("facebook"))
+  async facebookLogin(): Promise<any> {
+    return HttpStatus.OK;
+  }
+
+  @Get("/facebook/redirect")
+  @UseGuards(AuthGuard("facebook"))
+  async facebookLoginRedirect(@Req() req: AuthenticatedRequest): Promise<any> {
+    return {
+      statusCode: HttpStatus.OK,
+      data: req.user,
+    };
   }
 
   @Get('instagram')
